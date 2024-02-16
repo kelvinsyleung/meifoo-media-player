d278dd54e1714c045f64d4ba858e977da9f71478
kelvinsyleung <leungsikyuen@gmail.com>
2024-02-16 11:37:45 +0000

    feat(Button): update to use icon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import "./Button.scss";
 
-export interface ButtonProps {
-    label: string;
+interface ButtonProps {
+    label?: string;
+    onClick: () => void;
 }
 
-const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>;
+const Button: React.FC<React.PropsWithChildren<ButtonProps>> = ({
+    label,
+    onClick,
+    children,
+}) => {
+    return (
+        <button
+            className={`vp-btn${label ? " label" : ""}`}
+            data-label={label}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
